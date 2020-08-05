8624d4615016c81e4c11a7b540092cab3d336453
JixingFeng <2690587382@qq.com>
2020-08-05 23:41:08 +0800

    修改报错部分内容

--- src/main/java/com/example/mysecurity/service/auth/AuthUserDetailServiceImpl.java
+++ src/main/java/com/example/mysecurity/service/auth/AuthUserDetailServiceImpl.java
@@ -3,8 +3,11 @@ package com.example.mysecurity.service.auth;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-public class AuthUserDetailServiceImpl  implements UserDetailsService {
+@Service
+public class AuthUserDetailServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
         //查询用户信息
